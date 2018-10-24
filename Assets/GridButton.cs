bbf6dbbc0326f95a6cbf1a4c306f955dad1a2275
Kyle Wong <kylewonga@gmail.com>
2018-10-24 15:55:15 -0700

    Added basic button grid

---
+++ Assets/GridButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridButton : MonoBehaviour {
+
+    // Use this for initialization
+    public Model model;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
