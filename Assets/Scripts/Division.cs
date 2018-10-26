fc45ee2edd1293485fad063cdecf850febd8c484
Jack Sun <syy1125@hotmail.com>
2018-10-26 16:17:57 -0700

    Dummy implementation of Transmitter and Receiver
    Add dummy implementations for those classes so that the scenes compile.

---
+++ Assets/Scripts/Division.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Division : Operator {
+
+	protected override void Step() {
+		GetFromReceiver();
+		result = num1 / num2;
+		SendToTransmitter();
+	}
+}
---
+++ Assets/Scripts/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receiver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int GetNum1()
+	{
+		return 0;
+	}
+
+	public int GetNum2()
+	{
+		return 0;
+	}
+}
