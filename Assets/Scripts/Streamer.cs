3ad7d44a0c6329035915d975386cab44f31ad8c5
Kyle Wong <kylewonga@gmail.com>
2018-10-26 17:39:23 -0700

    Created main menu scene and added a thing

---
+++ Assets/Scripts/Streamer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Streamer : MonoBehaviour {
+
+    // Use this for initialization
+    private Vector2 velocity;
+    public float rightVelocity;
+    public float orthogonalVelocity;
+    public float orthogonalChance;
+    public float moveDuration;
+    private float moveTimer;
+    public float lifeTime;
+	void Start () {
+        Destroy(gameObject, lifeTime);
+        moveTimer = moveDuration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(moveTimer < 0)
+        {
+            moveTimer = moveDuration;
+            float rng = Random.Range(0f, 1f);
+            if(rng < orthogonalChance)
+            {
+                if(rng < orthogonalChance / 2)
+                {
+                    velocity = new Vector2(rightVelocity, orthogonalVelocity);
+                } else
+                {
+                    velocity = new Vector2(rightVelocity, -orthogonalVelocity);
+                }
+            } else
+            {
+                velocity = Vector2.right * rightVelocity;
+            }
+        } else
+        {
+            moveTimer -= Time.deltaTime;
+        }
+        transform.position += (Vector3)velocity*Time.deltaTime;
+	}
+}
