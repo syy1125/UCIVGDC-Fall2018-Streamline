aee1c16faa1ec1408978d6e383740189e407b4c6
Kyle Wong <kylewonga@gmail.com>
2018-11-07 16:19:24 -0800

    Added value displays for grid components

---
+++ Assets/Scripts/ValueDisplayManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueDisplayManager : MonoBehaviour {
+
+    // Use this for initialization
+    public static List<ValueDisplay> ValueDisplays = new List<ValueDisplay>();
+    public Transform displayPrefab;
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*  FOR TESTING PURPOSES
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AddAllValueDisplays();  
+        }
+        */
+	}
+    public void AddAllValueDisplays()
+    {
+        GameObject g = null;
+        for(int i = 0; i < Grid.Instance.Width; i++)
+        {
+            for (int j = 0; j < Grid.Instance.Height; j++)
+            {
+                g = Grid.Instance.GetGridComponent(i, j);
+                if (Grid.Instance.IsWire(g) || Grid.Instance.IsOperator(g))
+                {
+                    AddValueDisplay(g.transform);
+                }
+            }
+        }
+    }
+    public void AddValueDisplay(Transform target)
+    {
+        Transform vDisplay = Instantiate(displayPrefab, target);
+        ValueDisplays.Add(vDisplay.GetComponent<ValueDisplay>());
+    }
+    public void DestroyAllValueDisplays()
+    {
+        GameObject g = null;
+        for(int i = ValueDisplays.Count-1; i >= 0; i--)
+        {
+            g = ValueDisplays[i].gameObject;
+            ValueDisplays.RemoveAt(i);
+            Destroy(g);
+        }
+    }
+
+}
