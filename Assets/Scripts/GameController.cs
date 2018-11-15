82fa2c7ac463afc072d52f1ae9a641d733fe17d0
Jack Sun <syy1125@hotmail.com>
2018-11-14 16:35:10 -0800

    Code cleanup
    Refactor to remove some unused imports

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public enum SimState
 {
     EDITING,RUNNING,PAUSED,CRASHED
