126213e92743f29b8d26bad4d493c393b328ba3e
Jack Sun <syy1125@hotmail.com>
2018-11-14 17:17:59 -0800

    Move GameLevel to its own file

---
+++ Assets/Scripts/LevelSelect/GameLevel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+[Serializable]
+public class LevelTest
+{
+	public int[] Input1 = { };
+	public int[] Input2 = { };
+	public int[] Output1 = { };
+	public int[] Output2 = { };
+}
+
+[Serializable]
+public class GameLevel
+{
+	[NonSerialized]
+	public string FileName;
+
+	public string Name = "Level";
+
+	// Description is an overview of the level.
+	public string[] Description = new string[0];
+
+	// Objective is the task the player needs to achieve in a level.
+	public string[] Objective = null;
+	// Objective may be null, in which case it defaults to the level's description.
+
+	public LevelTest[] Tests = new LevelTest[0];
+
+	public static int Compare(GameLevel left, GameLevel right)
+	{
+		return string.CompareOrdinal(left.Name, right.Name);
+	}
+}
--- Assets/Scripts/LevelSelect/LevelList.cs
+++ Assets/Scripts/LevelSelect/LevelList.cs
@@ -1,45 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using Debug = UnityEngine.Debug;
-
-[Serializable]
-public class LevelTest
-{
-	public int[] Input1 = {};
-	public int[] Input2 = {};
-	public int[] Output1 = {};
-	public int[] Output2 = {};
-}
-
-[Serializable]
-public class GameLevel
-{
-	[NonSerialized]
-	public string FileName;
-
-	public string Name = "Level";
-
-	// Description is an overview of the level.
-	public string[] Description = new string[0];
-
-	// Objective is the task the player needs to achieve in a level.
-	public string[] Objective = null;
-	// Objective may be null, in which case it defaults to the level's description.
-
-	public LevelTest[] Tests = new LevelTest[0];
-
-	public static int Compare(GameLevel left, GameLevel right)
-	{
-		return string.CompareOrdinal(left.Name, right.Name);
-	}
-}
 
 public class LevelListState
 {
