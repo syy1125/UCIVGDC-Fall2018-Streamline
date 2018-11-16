7b9f431b81d9a513d4d8cee135b5a892cb2db1d0
Jack Sun <syy1125@hotmail.com>
2018-11-15 23:28:26 -0800

    Implement wire tutorial
    Implement tutorial for the first level.

---
+++ Assets/Scripts/Tutorials/WireTutorial.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WireTutorial : Tutorial
+{
+	private IEnumerator Start()
+	{
+		yield return new WaitUntil(() =>
+			ComponentSelection.selected == ComponentType.REDWIRE
+			|| ComponentSelection.selected == ComponentType.GREENWIRE);
+
+		Next();
+
+		Grid grid = Grid.Instance;
+		yield return new WaitUntil(() =>
+		{
+			for (int x = 1; x <= 6; x++)
+			{
+				if (grid.GetGridComponent(x, grid.Height - 1) == null)
+				{
+					return false;
+				}
+
+				if (!grid.IsWire(grid.GetGridComponent(x, grid.Height - 1)))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		});
+
+		Next();
+		
+		yield return new WaitUntil(() =>
+		{
+			for (int x = 1; x <= 6; x++)
+			{
+				if (grid.GetGridComponent(x, 0) == null)
+				{
+					return false;
+				}
+
+				if (!grid.IsWire(grid.GetGridComponent(x, 0)))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		});
+
+		Next();
+	}
+}
