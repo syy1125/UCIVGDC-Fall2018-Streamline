cc12bee2541855b6d8b1fcd93bc04b916fdc5781
Kyle Wong <kylewonga@gmail.com>
2018-12-01 00:18:48 -0800

    Added simState display UI

---
+++ Assets/Scripts/SimStateDisplay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimStateDisplay : MonoBehaviour {
+
+    // Use this for initialization
+    public Color EditingColor;
+    public Color RunningColor;
+    public Color PausedColor;
+    public Color CrashedColor;
+    public Color WinColor;
+    private Text MyText;
+	void Awake () {
+        MyText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MyText.text = "Status: ";
+        if(GameController.levelWon)
+        {
+            MyText.text += "<color=#" + ColorToHex(WinColor)+">";
+            MyText.text += "LEVEL WON";
+        } else
+        {
+            switch(GameController.simState)
+            {
+                case SimState.EDITING:
+                    MyText.text += "<color=#" + ColorToHex(EditingColor) + ">";
+                    break;
+                case SimState.RUNNING:
+                    MyText.text += "<color=#" + ColorToHex(RunningColor) + ">";
+                    break;
+                case SimState.PAUSED:
+                    MyText.text += "<color=#" + ColorToHex(PausedColor) + ">";
+                    break;
+                case SimState.CRASHED:
+                    MyText.text += "<color=#" + ColorToHex(CrashedColor) + ">";
+                    break;
+
+            }
+            MyText.text += GameController.simState.ToString();
+        }
+        MyText.text += "</color>";
+    }
+    private string ColorToHex(Color color)
+    {
+        string result = "";
+        result += ((int)(color.r * 255)).ToString("X2");
+        result += ((int)(color.g * 255)).ToString("X2");
+        result += ((int)(color.b * 255)).ToString("X2");
+        return result;
+
+    }
+}
---
+++ Assets/Scripts/TestCompletionDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TestCompletionDisplay : MonoBehaviour {
+
+    // Use this for initialization
+    private Text MyText;
+    private int TestsComplete;
+	void Awake () {
+        MyText = GetComponent<Text>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TestsComplete = 0;
+        for(int i = 0; i < GameController.TestCaseCompletion.Length; i++)
+        {
+            if (GameController.TestCaseCompletion[i])
+                TestsComplete++;
+        }
+        MyText.text =
+            TestsComplete.ToString()
+            + "/"
+            + GameController.gameLevel.Tests.Length 
+            + " Tests Complete";
+    }
+}
